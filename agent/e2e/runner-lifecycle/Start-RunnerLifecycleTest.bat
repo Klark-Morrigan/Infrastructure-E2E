ac6c0782b6d4c11c870501f6efd0d67c841bd6f6
Vitaly Andrev <v.andreev@systtech.ru>
2026-06-17 13:44:05 +0700

    chore: update user name

--- agent/e2e/runner-lifecycle/Start-RunnerLifecycleTest.bat
+++ agent/e2e/runner-lifecycle/Start-RunnerLifecycleTest.bat
@@ -3,7 +3,7 @@
 set APP_ID=3552893
 set RUNNERS_INSTALLATION_ID=128315873
 set PRIVATE_KEY_PATH="C:\Users\Klark Morgan\Code\Infrastructure-E2E\infrastructure-e2e.2026-04-30.private-key.pem"
-set OWNER="VitaliiAndreev"
+set OWNER="Klark-Morrigan"
 
 pwsh -NoProfile -ExecutionPolicy Bypass -File "%~dp0Start-RunnerLifecycleTest.ps1" ^
     -AppId                 %APP_ID% ^
